c007a0fbef6b1a794d1cde8e2369bc5ad78807b0
Phuritchayaphat Suphan <Phuritchayaphat.s@ku.th>
2026-03-03 23:19:40 +0700

    books edit

--- book.py
+++ book.py
@@ -4,9 +4,62 @@ app = Flask(__name__)
 
 # Sample data (in-memory database for simplicity)
 books = [
-    {"id": 1, "title": "Book 1", "author": "Author 1"},
-    {"id": 2, "title": "Book 2", "author": "Author 2"},
-    {"id": 3, "title": "Book 3", "author": "Author 3"}
+    {
+        "id": 1,
+        "title": "Frozen",
+        "author": "Jennifer Lee",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/0/05/Frozen_%282013_film%29_poster.jpg",
+        "price": 19.99
+    },
+    {
+        "id": 2,
+        "title": "The Lion King",
+        "author": "Roger Allers",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",
+        "price": 17.99
+    },
+    {
+        "id": 3,
+        "title": "Beauty and the Beast",
+        "author": "Gary Trousdale",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/e/e9/Beauty_and_the_Beast_1991_poster.jpg",
+        "price": 16.50
+    },
+    {
+        "id": 4,
+        "title": "Aladdin",
+        "author": "Ron Clements",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/5/58/Aladdinposter.jpg",
+        "price": 15.75
+    },
+    {
+        "id": 5,
+        "title": "Moana",
+        "author": "John Musker",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/2/26/Moana_Teaser_Poster.jpg",
+        "price": 18.25
+    },
+    {
+        "id": 6,
+        "title": "Tangled",
+        "author": "Nathan Greno",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/a/a8/Tangled_poster.jpg",
+        "price": 14.99
+    },
+    {
+        "id": 7,
+        "title": "Encanto",
+        "author": "Jared Bush",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/1/15/Encanto_poster.jpg",
+        "price": 20.00
+    },
+    {
+        "id": 8,
+        "title": "Cinderella",
+        "author": "Wilfred Jackson",
+        "image_url": "https://upload.wikimedia.org/wikipedia/en/5/54/Cinderella_%281950_film%29.jpg",
+        "price": 13.99
+    }
 ]
 
 # Create (POST) operation
